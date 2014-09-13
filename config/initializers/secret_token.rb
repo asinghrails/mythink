281a8b306f5d38e9d515e4f2a9b1c4605bc195f0
Anil singh <anil.singh826@gmail.com>
2014-09-13 13:42:57 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mythink::Application.config.secret_key_base = 'f24930bdccf2804c35ccd920bcbae1f5391b8553e6d69750af2ec8843493439a5f9968d298ba634f9e22330d06aa4249a6779ed73ed6e06af04f7d3bc46e264f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mythink::Application.config.session_store :cookie_store, key: '_mythink_session'
